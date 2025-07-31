d26bbdcc0830bb8bfd3b522f13fdaa401e4c0cbe
rohitsinghwho <rohiisingh8655@gmail.com>
2025-07-31 14:04:20 +0530

    first commit complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count,setCount]=useState(0);
   return (
-    <div>App</div>
+    <div 
+    style={{
+      display:"flex",
+      flexDirection:"column",
+      gap:"20px"
+    }}
+    >
+      <div>
+        <h1>Counter App</h1>
+        <span>Count: {count}</span>
+      </div>
+      <div>
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <button onClick={()=>setCount(count-1)}>Decrement</button>
+      </div>
+    </div>
   )
 }
 
