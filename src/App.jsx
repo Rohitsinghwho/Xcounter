40a2ed7a45ce1b311d00b94bb05d059850f25386
rohitsinghwho <rohiisingh8655@gmail.com>
2025-08-09 00:05:09 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import React, { useState } from 'react'
+import React, { Component} from "react";
 
-const App = () => {
-  const [count,setCount]=useState(0);
-  return (
-    <div 
-    style={{
-      display:"flex",
-      flexDirection:"column",
-      gap:"20px"
-    }}
-    >
-      <div>
-        <h1>Counter App</h1>
-        <span>Count: {count}</span>
-      </div>
-      <div>
-        <button onClick={()=>setCount(count+1)}>Increment</button>
-        <button onClick={()=>setCount(count-1)}>Decrement</button>
-      </div>
+export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      count:0
+    }
+    };
+    
+    increment=()=>{
+      this.setState((prevState)=>({count:prevState.count+1}));
+    };
+
+    decrement=()=>{
+      this.setState((prevState)=>({count:prevState.count-1}));
+    };
+
+  render(){return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {this.state.count}</p>
+      <button onClick={this.increment}>Increment</button>
+      <button onClick={this.decrement}>Decrement</button>
     </div>
-  )
-}
+  );
+}};
 
-export default App
+
